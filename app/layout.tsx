7f1313a10dcf65af539e8174a14b58cb36144cf2
jeftejb <jeftesambangojb@gmail.com>
2025-02-16 23:25:14 +0000

    update loading 003

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt">
+    <html lang="en">
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     </head>
