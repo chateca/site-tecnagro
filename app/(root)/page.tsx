7648ba0ebebb9dc0b5ac074f93176c7b6cf7351b
jeftejb <jeftesambangojb@gmail.com>
2025-02-20 00:05:25 +0000

    update all interfaces

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import BannerComponent from "@/components/BannerComponent";
 export default function Home() {
   return (
     <section  className="flex flex-col w-full">
-    
+     <BannerComponent/>
     </section>
   );
 }
