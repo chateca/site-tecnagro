94969d0bd387094fbd4a9f8ba5e2452a10f7b9de
jeftejb <jeftesambangojb@gmail.com>
2025-02-25 23:45:45 +0000

    update layout 023

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt">
  
       <body
         className={`${inter.variable} ${ibmPlaxSerif.variable}`}>
