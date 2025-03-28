f06ad53f31ef07c08a765cd4a4bcd108cb0350e3
jeftejb <jeftesambangojb@gmail.com>
2025-03-28 18:46:49 +0100

    commit 017

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 import type { NextConfig } from "next";
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com;
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self' data:;
-    connect-src 'self' https://api.example.com;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
 
 
 const nextConfig: NextConfig = {
@@ -33,19 +20,6 @@ const nextConfig: NextConfig = {
     
   },
 
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, " "),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
