fb3d9a7f8a77e5b34454c1fdfc58adafe445e620
jeftejb <jeftesambangojb@gmail.com>
2025-04-01 19:34:02 +0100

    update layout from admin pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 async function page() {
   const session = (await cookies()).get('session')?.value
-  if(session) return redirect('/admin')
+  if(session) return redirect('/adminPage')
   return (
      <div className='flex flex-col h-[100vh] w-full items-center justify-center'>
         <h1 className='text-green-1 font-extrabold text-[18px] uppercase' >Olá seja bem vindo!</h1>
